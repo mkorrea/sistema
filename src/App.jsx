624b7b1ccd342d0d101701e6e540069e45e78a99
mkorrea <mathscorrea@gmail.com>
2024-06-03 21:28:01 -0300

    create sistema project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './global.css'
 
+import { RoutesApp } from './routes'
+
+import { BrowserRouter } from 'react-router-dom'
+
 export function App() {
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
   )
 }
---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,7 @@
+export function SignIn() {
+   return (
+      <main>
+         <h1>Login</h1>
+      </main>
+   )
+}
