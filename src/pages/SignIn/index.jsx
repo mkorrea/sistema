19051ebdbe8aee35dc70ffe89bbd0c4acc0aa0f9
mkorrea <mathscorrea@gmail.com>
2024-06-05 14:04:26 -0300

    add components sign in page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,40 @@
+import './SignIn.css'
+
+import { useState } from 'react'
+
+import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom'
+
 export function SignIn() {
+
+   const [ email, setEmail ] = useState('')
+   const [ password, setPassword ] = useState('')
+
    return (
-      <main>
-         <h1>Login</h1>
-      </main>
+      <div className='container-center'>
+         <div className="login">
+            <div className="login-area">
+               <img src={logo} alt="Logo do sistema" />
+            </div>
+            <form>
+               <h1>Entrar</h1>
+               <input 
+                  type="text"
+                  placeholder='exemplo@email.com'
+                  value={email}
+                  onChange={ (e) => setEmail(e.target.value)}
+               />
+               <input 
+                  type="password"
+                  placeholder='******'
+                  value={password}
+                  onChange={ (e) => setPassword(e.target.value)}
+               />
+
+               <input type="submit" value="Acessar"/>
+            </form>
+            <Link to='/register'>Criar conta</Link>
+         </div>
+      </div>
    )
 }
