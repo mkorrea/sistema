0bc03133e64843854a855a7d1ad2752959f6f311
mkorrea <mathscorrea@gmail.com>
2024-06-19 15:46:18 -0300

    config sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,8 +1,36 @@
+import './sidebar.css'
+
+import avatarImg from '../../assets/avatar.png'
+
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/auth'
+import { Link } from 'react-router-dom'
+import { Home, Settings, User } from 'lucide-react'
 
 export function Sidebar() {
+   const { user } = useContext(AuthContext)
+    
    return (
-      <div>
-         <h1>Sidebar</h1>
+      <div className="sidebar">
+         <section>
+            <img src={ user.avatarUrl === null ? avatarImg : user.avatarUrl } alt="User photo" />
+            {/* <img src="https://github.com/mkorrea.png" alt="User photo" /> */}
+         </section>
+
+         <Link to='/dashboard'>
+            <Home color='#fff' size={24} />
+            Home
+         </Link>
+
+         <Link to='/customers'>
+            <User color='#fff' size={24} />
+            Customers
+         </Link>
+         
+         <Link to='/profile'>
+            <Settings color='#fff' size={24} />
+            Profile
+         </Link>
       </div>
    )
 }
