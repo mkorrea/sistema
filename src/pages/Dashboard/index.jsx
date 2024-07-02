a89ac2b2d915dc31b32e0ce47c0b5d4d6756bbfa
mkorrea <mathscorrea@gmail.com>
2024-07-02 00:02:58 -0300

    add load more tickets button

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -7,29 +7,35 @@ import { AuthContext } from "../../contexts/auth"
 import { Sidebar } from "../../components/Sidebar"
 import { Title } from "../../components/Title"
 
-import { Backpack, Edit, Edit2, MessageSquare, Plus, Search } from "lucide-react"
-import { collection, getDocs, limit, orderBy, query } from "firebase/firestore"
+import { Backpack, Edit, Edit2, Loader, MessageSquare, Plus, Search } from "lucide-react"
+import { collection, getDocs, limit, orderBy, query, startAfter } from "firebase/firestore"
 import { db } from "../../services/FirebaseConnections"
+import { format } from "date-fns"
 
 const listRef = collection(db, "tickets")
 
 export function Dashboard() {
-   const { logout } = useContext(AuthContext)
+   
    const [ tickets, setTickets ] = useState([])
    const [ loading, setLoading ] = useState(true)
+
    const [ isEmpty, setIsEmpty ] = useState(false)
+   const [ lastDoc, setLastDoc ] = useState()
+   const [ loadingMore, setLoadingMore ] = useState(false)
 
    useEffect(()=>{
       async function loadTickets() {
          const q = query(listRef, orderBy('created', 'desc'), limit(5))
 
          const querySnapshot = await getDocs(q)
+         setTickets([])
          updateState(querySnapshot)   
 
          setLoading(false)
       }
 
-      loadTickets()
+         loadTickets()
+      
 
       return ()=>{
 
@@ -45,6 +51,7 @@ export function Dashboard() {
             list.push({
                id: doc.id,
                created: doc.data().created,
+               createdFormat: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
                client: doc.data().client,
                clientId: doc.data().clientId,
                subject: doc.data().subject,
@@ -52,12 +59,45 @@ export function Dashboard() {
                status: doc.data().status
             })
          });
+         const lastDoc = querySnapshot.docs[querySnapshot.docs.length - 1] // taking last document
+         setLastDoc(lastDoc)
+         
          setTickets(tickets => [...tickets, ...list])
+         setIsEmpty(false)
       } else {
          setIsEmpty(true)
       }
+      setLoadingMore(false)
    }
    
+
+   async function handleSearchMore() {
+      setLoadingMore(true)
+
+      const q = query(listRef, orderBy('created', 'desc'), startAfter(lastDoc), limit(5))
+      const querySnapshot = await getDocs(q)
+      await updateState(querySnapshot)
+   }
+   
+   if(loading) {
+      return(
+         <div>
+            <Sidebar />
+         <div className="content">
+            <Title name="Dashboard" >
+               <MessageSquare size={25} />
+            </Title>
+         
+
+            <div className="container dashboard">
+               <span>Searching tickets...</span>
+               <Loader className='loading-icon'/>
+            </div>
+         </div>     
+
+         </div>
+      )
+   }
 
    return (
       <div>
@@ -101,14 +141,14 @@ export function Dashboard() {
                         {tickets.map((item, index)=>{
                            return(
                               <tr key={index}>
-                                 <td data-label="Cliente"> {item.client} </td>
+                                 <td data-label="Client"> {item.client} </td>
                                  <td data-label="Topic"> {item.subject} </td>
                                  <td data-label="Status">
-                                    <span className="badge" style={{ background: 'var(--gray400)' }}>
+                                    <span className="badge" style={{ background: item.status === 'Open' ? 'var(--green500)' : 'var(--gray400)' }}>
                                        {item.status}
                                     </span>
                                  </td>
-                                 <td data-label="Registered in"> 14/06/24 </td>
+                                 <td data-label="Registered in"> {item.createdFormat} </td>
                                  <td data-label="#"> 
                                        <button className="action" style={{background: 'var(--blue600)'}}>
                                        <Search color="#fff" size={17} /> 
@@ -122,26 +162,12 @@ export function Dashboard() {
                         })}
 
 
-                        <tr>
-                           <td data-label="Cliente"> Marcado Litoral </td>
-                           <td data-label="Topic"> Suporte </td>
-                           <td data-label="Status">
-                              <span className="badge" style={{ background: 'var(--gray400)' }}>
-                                 Open
-                              </span>
-                           </td>
-                           <td data-label="Registered in"> 14/06/24 </td>
-                           <td data-label="#"> 
-                              <button className="action" style={{background: 'var(--blue600)'}}>
-                                 <Search color="#fff" size={17} /> 
-                              </button>  
-                              <button className="action" style={{background: 'var(--yellow500)'}}>
-                                 <Edit2 color="#fff" size={17} />
-                              </button>
-                           </td>
-                        </tr>
                      </tbody>
                   </table>
+
+                  { loadingMore && <h3> Searching more tickets... </h3> }
+                  { !loadingMore && !isEmpty && <button className="btn-more" onClick={ handleSearchMore }> Search more tickets </button> }
+                  
                   </>
                )}
 
